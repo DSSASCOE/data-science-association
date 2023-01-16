f4a20280d4afb2ad654f36b6d7ac28c2626f64d3
Manishsingh1110 <dsmanishsingh@gmail.com>
2023-01-16 21:05:50 +0530

    first commit

---
+++ src/data/Nakshatraphoto.js
@@ -0,0 +1,12 @@
+var Nakshatraphoto = [
+    {
+        "src": "https://i.postimg.cc/V6mVN2q5/wallpainting-3.jpg",
+        "category": "events"
+    },
+    {
+        "src": "https://i.postimg.cc/C176nJ77/wallpainting-5.jpg",
+        "category": "events"
+    }
+]
+
+export default Nakshatraphoto
